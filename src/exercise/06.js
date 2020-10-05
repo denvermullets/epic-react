c381005e95e04d803e6074e36c76e182c64e6a34
denvermullets <devmullets@gmail.com>
2020-10-05 12:40:09 -0400

    :recycle: finish lesson 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,11 @@ import React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
+  function handleSubmit(e) {
+    e.preventDefault()
+    const eventVal = e.target.elements.usernameInput.value
+    onSubmitUsername(eventVal)
+  }
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -18,11 +23,13 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input by specifying an `id` on
   // the input and a matching value as an `htmlFor` prop on the label.
+
+  // using id is better for accessibility
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor='usernameInput'>Username:</label>
+        <input id='usernameInput' type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
